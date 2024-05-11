4d02852523df9c0b48c2e3e7bf404230c0049741
= <=>
2024-05-11 09:20:50 +0300

    .

--- process_scheduling_algorithm/Program.cs
+++ process_scheduling_algorithm/Program.cs
@@ -14,31 +14,6 @@ namespace process_scheduling_algorithm
         [STAThread]
         static void Main()
         {
-            //List<Process> ps = new List<Process>() {
-            //        new Process(0, 10, 3),
-            //        new Process(0, 1, 1),
-            //        new Process(0, 2, 4),
-            //        new Process(0, 1, 5),
-            //        new Process(0, 5, 2),
-            //};
-
-            //Scheduling_Algo.Result res = Scheduling_Algo.Priority(ref ps, Scheduling_Algo.Type.NonPreemptive);
-
-
-            //foreach (Process p in ps)
-            //{
-            //    Console.WriteLine(p.ToString());
-            //}
-
-            //Console.WriteLine(res.ToString());
-
-
-
-
-
-
-
-
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
